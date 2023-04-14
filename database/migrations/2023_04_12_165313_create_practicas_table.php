745809c4695d499803aca8b9770edece0dc3545d
VictorMoisesHonorCastro <73166294+VictorMoisesHonorCastro@users.noreply.github.com>
2023-04-14 20:18:33 +0200

    Add files via upload

---
+++ database/migrations/2023_04_12_165313_create_practicas_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('practicas', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_profesor')->references('id')->on('users')->nullable();
+            $table->string('codigo');
+            $table->string('nombre')->nullable();
+            $table->longText('descripcion');
+            $table->string('puntuacion');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('practicas');
+    }
+};
