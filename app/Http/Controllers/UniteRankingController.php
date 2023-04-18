4fef3a9ff687bf1a99503f730c4220a4381e9724
VictorMoisesHonorCastro <73166294+VictorMoisesHonorCastro@users.noreply.github.com>
2023-04-18 19:53:14 +0200

    Add files via upload

--- app/Http/Controllers/UniteRankingController.php
+++ app/Http/Controllers/UniteRankingController.php
@@ -8,60 +8,51 @@ use Illuminate\Support\Facades\DB;
 
 class UniteRankingController extends Controller
 {
-    public function indexa(Request $request, $mote)
-    {
-        $sql = "SELECT users.id, users.mote, unite_rankings.codigo, unite_rankings.puntos, create_rankings.nombre
-                FROM users, unite_rankings, create_rankings
+    public function indexa (Request $request,$mote){
+        $sql = "SELECT users.id, users.mote, unite_rankings.codigo, unite_rankings.puntos
+                FROM users, unite_rankings
                 WHERE users.id = unite_rankings.id_usuario
-                AND users.mote='$request->mote' 
-                AND unite_rankings.codigo=create_rankings.codigo;";
+                AND users.mote='$mote';";
         $CreateRanking = DB::select($sql);
         return $CreateRanking;
     }
-
-    public function indexall(Request $request, $codigo)
-    {
-        $sql2 = "SELECT users.mote, users.name, users.lastname, unite_rankings.puntos, unite_rankings.codigo
+    public function indexall(Request $request,$codigo){
+        $sql2 = "SELECT users.mote, unite_rankings.puntos
                 FROM users, unite_rankings
                 WHERE users.id = unite_rankings.id_usuario
-                AND unite_rankings.codigo= '$request->codigo';";
-        $viewranking = DB::select($sql2);
+                AND unite_rankings.codigo= '$codigo';";
+        $viewranking=DB::select($sql2);
         return $viewranking;
     }
-
-    public function unitedranking(Request $request)
-    {
+    public function unitedranking(Request $request,$mote,$codigo){
         $request->validate([
-            'codigo' => 'required',
-            'id_usuario' => 'required',
+            'codigo'=>'',
+            'id_usuario'=>'required',
         ]);
-
         $UniteRanking = new UniteRanking();
-        $UniteRanking->codigo = $request->codigo;
-        $UniteRanking->id_usuario = $request->id_usuario;
+        $UniteRanking->id_ranking=$request->id_ranking;
+        $UniteRanking->codigo=$request->codigo;
+        $UniteRanking->puntos=$request->puntos;
+        $UniteRanking->id_usuario=$request->id_usuario;
 
-        $sql = "SELECT codigo
+        $sql="SELECT codigo
                 FROM create_rankings
-                WHERE codigo = '$request->codigo';";
-
+                WHERE codigo = '$codigo';";
         $consul = DB::select($sql);
-
-        if (count($consul) == 1) {
-
+        if(count($consul)==1){
             $sql1 = "SELECT users.id, users.mote, unite_rankings.codigo, unite_rankings.puntos
             FROM users, unite_rankings
             WHERE users.id = unite_rankings.id_usuario
-            AND users.mote='$request->mote'
-            AND unite_rankings.codigo='$request->codigo';";
-
+            AND users.mote='$mote'
+            AND unite_rankings.codigo='$codigo';";
             $consul2 = DB::select($sql1);
 
-            if (count($consul2) == 1) {
+            if(count($consul2)==1 ){
                 return response()->json([
                     "status" => 1,
                     'message' => 'You are already in the ranking',
                 ]);
-            } else {
+            }else{
                 $UniteRanking->save();
                 return response()->json([
                     "status" => 1,
@@ -69,7 +60,7 @@ class UniteRankingController extends Controller
                     "value" => $UniteRanking
                 ]);
             }
-        } else {
+        }else{
             return response()->json([
                 "status" => 0,
                 'message' => 'Code not found',
