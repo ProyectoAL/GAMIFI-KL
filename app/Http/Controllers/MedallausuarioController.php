05f939caeb95c7459cde34913483ed57c385dc09
VictorMoisesHonorCastro <73166294+VictorMoisesHonorCastro@users.noreply.github.com>
2023-05-02 20:14:47 +0200

    Update MedallausuarioController.php

--- app/Http/Controllers/MedallausuarioController.php
+++ app/Http/Controllers/MedallausuarioController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 
 class MedallausuarioController extends Controller
 {
-    public function Indexall(){//Muestra todas las medallas de un usuario de un solo ranking
+       public function indexallmedaalauser(){//Muestra todas las medallas de un usuario de un solo ranking
 
     }
     public function createmedallauser(Request $request){//poner medallas a los usuarios
@@ -27,8 +27,30 @@ class MedallausuarioController extends Controller
             "value" => $createmedallauser
         ]);
     }
-    public function updatemedallausuario(){//actualizar las medallas de los usuarios
+    public function updatemedallausuario(Request $request, $id){//actualizar las medallas de los usuarios
+        $request->validate([
+            'id_usuario',
+            'id_ranking',
+            'id_medalla'
+            ]);
+            $user_id = $id;
+        if (medallausuario::where(["id_usuario" => $user_id])->exists()) {
+            $updatemedallausuario = medallausuario::find($user_id);
+            $updatemedallausuario->id_medalla= $request->id_medalla;
+            $updatemedallausuario->save();
+            return response()->json([
+                "status" => 1,
+                "message" => "Actualizado correctamente",
+                "value" => $updatemedallausuario
+            ]);
+        } else {
+            return response()->json([
+                "status" => 1,
+                "message" => "No se pùdo actucalizar",
+            ]);
+        }
     }
-    public function deletemedallausuario(){//eliminar las medallas de los usuarios
+    public function deletemedallausuario(Request $request){//eliminar las medallas de los usuarios
+        medallausuario::destroy($request->id);
     }
 }
