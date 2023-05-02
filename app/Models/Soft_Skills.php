97a79c40d03074f18d7f18c8c42246ea99d9ff23
VictorMoisesHonorCastro <73166294+VictorMoisesHonorCastro@users.noreply.github.com>
2023-05-02 19:52:02 +0200

    Add files via upload

---
+++ app/Models/Soft_Skills.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Soft_Skills extends Model
+{
+    use HasFactory;
+    public $timestamps=false;
+    protected $fillable = [
+        'rango',
+        'medalla'
+    ];
+}
