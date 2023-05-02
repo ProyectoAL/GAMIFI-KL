4fe5ed84cee2df6617b3bb164fa5fb93bb2d9226
VictorMoisesHonorCastro <73166294+VictorMoisesHonorCastro@users.noreply.github.com>
2023-05-02 19:52:54 +0200

    Add files via upload

---
+++ app/Http/Controllers/SoftSkillsController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Soft_Skills;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class SoftSkillsController extends Controller
+{
+    public function indexsoftskill(){
+        $Soft_Skills=Soft_Skills::all();
+        return $Soft_Skills;
+    }
+    public function createsoftskill(Request $request){//Poder crear las medallas de los softskill
+        $request->validate([
+            'rango' => 'Required',
+            'medalla' => ' image',
+            ]);
+        $createsoftskill= new Soft_Skills();
+        $createsoftskill->rango= $request->rango;
+        $createsoftskill->medalla= $request->medalla;
+        $createsoftskill->save();
+        return response()->json([
+            "status" => 1,
+            'message' => 'Successfully',
+            "value" => $createsoftskill
+        ]);
+    }
+    public function updatesoftskill(Request $request,$id){
+
+        $request->validate([
+            'rango' => 'Required',
+            'medalla' => 'image',
+        ]);
+
+        $user_id = $id;
+        if (Soft_Skills::where(["id" => $user_id])->exists()) {
+            $createsoftskill = Soft_Skills::find($user_id);
+            $createsoftskill->rango= $request->rango;
+            $createsoftskill->medalla= $request->medalla;
+            $createsoftskill->save();
+            return response()->json([
+                "status" => 1,
+                "message" => "Actualizado correctamente",
+                "value" => $createsoftskill
+            ]);
+        } else {
+            return response()->json([
+                "status" => 1,
+                "message" => "No se pùdo actucalizar",
+            ]);
+        }
+    }
+    public function deletesoftskill(Request $request){
+        $deletesoftskill = DB::table('soft__skills')
+        ->where('id', '=', $request->id)
+        ->delete();
+        return response()->json([
+            "status" => 0,
+            'message' => 'Soft_Skills deleted successfully',
+            "value" => $deletesoftskill
+        ]);
+    }
+}
