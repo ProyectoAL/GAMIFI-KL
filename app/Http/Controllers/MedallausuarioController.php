eb54f9bbe1c726c2627fd74a314c0904d5061baa
VictorMoisesHonorCastro <73166294+VictorMoisesHonorCastro@users.noreply.github.com>
2023-05-05 20:06:40 +0200

    Add files via upload

--- app/Http/Controllers/MedallausuarioController.php
+++ app/Http/Controllers/MedallausuarioController.php
@@ -17,9 +17,30 @@ class MedallausuarioController extends Controller
             'id_medalla'
             ]);
         $createmedallauser= new medallausuario();
-        $createmedallauser->id_usuario= $request->id_usuario;
         $createmedallauser->id_ranking= $request->id_ranking;
-        $createmedallauser->id_medalla= $request->id_medalla;
+        $createmedallauser->codigo= $request->codigo;
+        $createmedallauser->Responsabilidad=$request->Responsabilidad;
+        if(!empty($createmedallauser->Responsabilidad)){
+            $createmedallauser->Responsabilidad=26;
+        }
+        $createmedallauser->Cooperación=$request->Cooperación;
+        if(!empty($createmedallauser->Cooperación)){
+            $createmedallauser->Cooperación=26;
+        }
+        $createmedallauser->Autonomía_e_iniciativa=$request->Autonomía_e_iniciativa;
+        if(!empty($createmedallauser->Autonomía_e_iniciativa)){
+            $createmedallauser->Autonomía_e_iniciativa=26;
+        }
+        $createmedallauser->Gestión_emocional=$request->Gestión_emocional;
+        if(!empty($createmedallauser->Gestión_emocional)){
+            $createmedallauser->Gestión_emocional=26;
+        }
+        $createmedallauser->abilidades_de_pensamiento=$request->abilidades_de_pensamiento;
+        if(!empty($createmedallauser->abilidades_de_pensamiento)){
+            $createmedallauser->abilidades_de_pensamiento=26;
+        }
+        $createmedallauser->puntos_semanales=$request->puntos_semanales;
+        $createmedallauser->id_usuario= $request->id_usuario;
         $createmedallauser->save();
         return response()->json([
             "status" => 1,
@@ -27,19 +48,40 @@ class MedallausuarioController extends Controller
             "value" => $createmedallauser
         ]);
     }
-    public function updatemedallausuario(Request $request, $id){//actualizar las medallas de los usuarios
+    public function updatemedallaResponsabilidad(Request $request, $id){//actualizar las medallas de los usuarios
         $request->validate([
             'id_usuario',
             'id_ranking',
-            'Responsabilidad',
-            'Cooperación',
-            'Autonomía e iniciativa',
-            'Gestión emocional',
-            'Habilidades de pensamiento'
+            'Responsabilidad'
             ]);
-            $user_id = $id;
-            $sql="Sele ";
-
+    }
+    public function updatemedallaCooperación(Request $request, $id){//actualizar las medallas de los usuarios
+        $request->validate([
+            'id_usuario',
+            'id_ranking',
+            'Cooperación'
+            ]);
+    }
+    public function updatemedallaAutonomía_e_iniciativa(Request $request, $id){//actualizar las medallas de los usuarios
+        $request->validate([
+            'id_usuario',
+            'id_ranking',
+            'Autonomía_e_iniciativa'
+            ]);
+    }
+    public function updatemedallaGestión_emocional(Request $request, $id){//actualizar las medallas de los usuarios
+        $request->validate([
+            'id_usuario',
+            'id_ranking',
+            'Gestión_emocional'
+            ]);
+    }
+    public function updatemedallaHabilidades_de_pensamiento(Request $request, $id){//actualizar las medallas de los usuarios
+        $request->validate([
+            'id_usuario',
+            'id_ranking',
+            'Habilidades_de_pensamiento'
+            ]);
     }
     public  function updatetrigger(Request $request){
         $trigger="
