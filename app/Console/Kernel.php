eedab429d67a7e397bda5999e04d8ab2e9ac7e27
VictorMoisesHonorCastro <73166294+VictorMoisesHonorCastro@users.noreply.github.com>
2023-05-10 17:28:17 +0200

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,6 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->command('app:testtask')->weeklyOn(1, '8:00');
     }
 
     /**
@@ -23,5 +24,6 @@ class Kernel extends ConsoleKernel
         $this->load(__DIR__.'/Commands');
 
         require base_path('routes/console.php');
+        commands\testtask::class;
     }
 }
