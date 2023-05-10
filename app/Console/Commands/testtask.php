b4dcee0fbe3a8bf97eed96b672170ddb7251f4a8
VictorMoisesHonorCastro <73166294+VictorMoisesHonorCastro@users.noreply.github.com>
2023-05-10 17:14:32 +0200

    Add files via upload

---
+++ app/Console/Commands/testtask.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Console\Scheduling\Schedule;
+
+class testtask extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:testtask';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(Schedule $schedule): void
+    {
+        $totalUsers = DB::table('unite_rankings')
+                ->update(['puntos_semanales' => '1000']);
+    }
+}
